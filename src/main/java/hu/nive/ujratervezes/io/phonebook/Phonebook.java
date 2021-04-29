20b08e8e430067effd02278a49af73dace9cd1ba
kubanekzsolt <63916757+kubanekzsolt@users.noreply.github.com>
2021-04-29 12:08:36 +0200

    Phonebook update
    File management

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,43 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Map;
+
+public class Phonebook {
+
+	public void exportPhonebook(Map<String, String> contacts, String output) {
+		if (contacts == null || output == null) {
+			throw new IllegalArgumentException("Some of the parameters is null!");
+		}
+		Path path = Paths.get(output);
+		clean(path);
+		String prefix = "";
+		for (Map.Entry<String, String> entry: contacts.entrySet()) {
+			try {
+				Files.write(path, 
+						(prefix + entry.getKey() + ": " + entry.getValue())
+							.getBytes(),
+							StandardOpenOption.APPEND);
+				prefix = "\n";
+			} catch (IOException e) {
+			}
+		}
+	}
+
+	private void clean(Path path) {
+		try {
+			Files.delete(path);
+		} catch (IOException e1) {
+		}
+		try {
+			Files.createFile(path);
+		} catch (IOException e1) {
+		}
+	}
+
+	
+}
